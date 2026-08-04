2adcb12ff20710e2e169c378c40ca50edaa8f567
Nancy-sharma01 <nancysharma11bh@gmail.com>
2026-08-04 23:23:30 +0530

    Add insert node at head of the linked list solution

---
+++ Linked_list/Hacker_Rank/day-32/02_Insert_Node_at_Head.py
@@ -0,0 +1,10 @@
+"""
+Problem:
+Insert a node at the beginning of a singly linked list.
+Platform: HackerRank
+"""
+
+def insertNodeAtHead(head, data):
+    new_node = SinglyLinkedListNode(data)
+    new_node.next = head
+    return new_node
